df421248996436b916182bfb8788a77e0ed0e743
Sofiia13 <sophiyastanish@gmail.com>
2025-05-27 17:07:00 +0300

    change condition

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -36,7 +36,7 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(victim) {
-    if (!(victim instanceof Carnivore) && !victim.hidden) {
+    if (victim instanceof Herbivore && !victim.hidden && !victim.hidden) {
       victim.health -= 50;
     }
   }
